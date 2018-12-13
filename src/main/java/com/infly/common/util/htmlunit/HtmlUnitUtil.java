f254b2f295edafd2f2d1c41cf1bd3d22a1133aac
周龙 <zlong@travelsky.com>
2018-12-13 14:50:48 +0800

    parse for class

--- src/main/java/com/infly/common/util/htmlunit/HtmlUnitUtil.java
+++ src/main/java/com/infly/common/util/htmlunit/HtmlUnitUtil.java
@@ -1,4 +1,4 @@
-package htmlunit;
+package com.infly.common.util.htmlunit;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -7,6 +7,8 @@ import java.util.Map.Entry;
 import java.util.Properties;
 import com.gargoylesoftware.htmlunit.NicelyResynchronizingAjaxController;
 import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.DomElement;
+import com.gargoylesoftware.htmlunit.html.DomNodeList;
 import com.gargoylesoftware.htmlunit.html.HtmlElement;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
@@ -37,6 +39,40 @@ public class HtmlUnitUtil {
         String total = resultElmt.getAttribute("data-total");
         String items = resultElmt.getAttribute("data-results");
         list.add(item.getValue().toString() + "," + total + "," + items);
+      }
+      for (String str : list) {
+        System.out.println(str);
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    } finally {
+      webClient.close();
+    }
+  }
+  
+  public static void parseHtmlResultByClass() {
+    Properties properties = new Properties();
+    final WebClient webClient = new WebClient();
+    try {
+      properties.load(HtmlUnitUtil.class.getResourceAsStream("/config.properties"));
+      List<String> list = new ArrayList<String>();
+      for (Entry<Object, Object> item : properties.entrySet()) {
+        webClient.getOptions().setJavaScriptEnabled(true);
+        webClient.getOptions().setActiveXNative(true);
+        webClient.getOptions().setCssEnabled(false);
+        webClient.getOptions().setThrowExceptionOnScriptError(false);
+        webClient.setAjaxController(new NicelyResynchronizingAjaxController());
+        webClient.waitForBackgroundJavaScript(10 * 1000);
+
+        HtmlPage page = webClient.getPage(URL + item.getValue().toString());
+        DomNodeList<DomElement> elementsByTagNameSubmit = page.getElementsByTagName("input");
+        for (DomElement domElement : elementsByTagNameSubmit) {
+            if (domElement.getAttribute("class").contains("shadow")
+                    || domElement.getAttribute("type").contains("submit")) {
+                System.out.println(domElement.getTextContent());
+                break;
+            }
+        }
       }
       for (String str : list) {
         System.out.println(str);
